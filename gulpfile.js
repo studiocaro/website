7c959e0be51640fc208fe7732da2b1cd4355624c
Caroline Vermeir <hello@studiocaro.be>
2015-06-12 11:43:08 +0200

    Made splashpage and css for it

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,27 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('styles', function() {
+    gulp.src('sass/**/*.scss')
+        .pipe(sass({
+            errLogToConsole: true
+        }))
+        .pipe(gulp.dest('./dist/css'));
+});
+
+//Watch task
+gulp.task('default',function() {
+    gulp.watch('sass/**/*.scss',['styles']);
+});
 
 gulp.task('default', function() {
   // place code for your default task here
 })fy = require('gulp-uglify');
 
 gulp.task('compress', function() {
-  return gulp.src('lib/*.js')
+  return gulp.src('js/*.js')
     .pipe(uglify())
-    .pipe(gulp.dest('dist'));
+    .pipe(gulp.dest('dist/js'));
 });
 
 
